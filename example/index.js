85b431704087499d1145e249620241a9bf994650
vortesnail <1091331061@qq.com>
2019-10-31 16:19:48 +0800

    feat: code refactoring

--- example/index.js
+++ example/index.js
@@ -3,12 +3,15 @@ let box = document.querySelector('.box');
 function hiddenTestBox() {
   let textBox = document.querySelector('.test-box');
   textBox.style.display = 'none';
+  box.style.cursor = 'none';
 }
 
 function showTestBox() {
   let textBox = document.querySelector('.test-box');
   textBox.style.display = 'block';
+  box.style.cursor = 'default';
 }
 
-let hoversd = new window.HoverSD();
-hoversd.secondsHoverEX(box, hiddenTestBox, 2000, showTestBox);
+let hoversd = new window.HoverSD(box, hiddenTestBox, 2000, showTestBox);
+hoversd.secondsHoverEX();
+// hoversd.removeElemEventListener();
--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-function HoverSD() {
+function HoverSD(element, secondsLaterDoFn, seconds, reNormalFn) {
   var timeout;
+  var ele = element, secondsLDF = secondsLaterDoFn, secs = seconds, reNFn = reNormalFn;
 
-  const rebuildTimer = function(secondsLaterDoFn, seconds, reNormalFn) {
-    return function() {
-      var context = this;
-      var args = arguments;
-      reNormalFn.apply(context, args);
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        secondsLaterDoFn.apply(context, args);
-      }, seconds);
-    }
+  var rebuildTimer = function() {
+    var context = this;
+    var args = arguments;
+    reNFn.apply(context, args);
+    clearTimer();
+    timeout = setTimeout(() => {
+      secondsLDF.apply(context, args);
+    }, secs);
   }
   
-  const clearTimer = function() {
+  var clearTimer = function() {
     timeout && clearTimeout(timeout);
   }
 
-  this.secondsHoverEX = function(element, secondsLaterDoFn, seconds, reNormalFn) {
-    element.addEventListener('mousemove', rebuildTimer(secondsLaterDoFn, seconds, reNormalFn));
-    element.addEventListener('mouseleave', clearTimer);
+  this.secondsHoverEX = function() {
+    ele.addEventListener('mousemove', rebuildTimer);
+    ele.addEventListener('mouseleave', clearTimer);
   }
   
-  
   this.removeElemEventListener = function() {
-    
+    ele.removeEventListener('mousemove', rebuildTimer);
+    ele.removeEventListener('mouseleave', clearTimer);
   }
 }
 
